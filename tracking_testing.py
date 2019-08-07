e8a93f855e787f49ef1d4f88d7bffc9209e2b539
Vlad <twixstar1@gmail.com>
2019-08-07 05:34:49 +0100

    new reference

--- tracking_testing.py
+++ tracking_testing.py
@@ -3,72 +3,171 @@ from mvnc import mvncapi as mvnc
 import cv2
 import os
 import time
+import numpy as np
 
 from scipy.spatial import distance as dist
 from collections import OrderedDict
 
-from pyimagesearch.centroidtracker import CentroidTracker
+from pyimagesearch.centroidtracker_mine import CentroidTracker
+import data as Data_support
 import support_functions as sf
 
-PREPROCESS_DIMS = (300, 300)
+PERSON_GRAPH = 'graphs/g_300' #300
+FACE_GRAPH = 'graphs/ssd-face' #300
+AGE_GRAPH = 'graphs/age_model' #227 +mean
+GENDER_GRAPH = 'graphs/gender_model' #227 +mean
 
-GRAPH_FILEPATH = 'graphs/g_300'
+AGE_GRAPH_LIST = ['(0, 2)','(4, 6)','(8, 12)','(15, 20)','(25, 32)','(38, 43)','(48, 53)','(60, 100)']
+GENDER_GRAPH_LIST = ['Male','Female']
 
+PREPROCESS_DIMS_300 = (300, 300)
+PREPROCESS_DIMS_227 = (227, 227)
+
+ilsvrc_mean = np.load('graphs/age_gender_mean.npy').mean(1).mean(1) #loading the mean file
+
+#OPEN DEVICE
 device_list = mvnc.enumerate_devices()
-device = mvnc.Device(device_list[0])
-device.open()
+if device_list:
+    device = mvnc.Device(device_list[0])
+    device.open()
+    print("Found {} device(s) and connected to {}.".format(len(device_list),device))
+else:
+    print("0 devices found, please make sure the NCS is connected. Exiting...")
+    sys.exit()
 
-with open(GRAPH_FILEPATH, mode='rb') as f: graph_buffer = f.read()
-graph = mvnc.Graph('graph')
+#ALLOCATING GRAPHS
+with open(PERSON_GRAPH, mode='rb') as f: person_graph_buffer = f.read()
+person_graph = mvnc.Graph('person_graph')
+input_fifo_person, output_fifo_person = person_graph.allocate_with_fifos(device, person_graph_buffer)
 
-input_fifo, output_fifo = graph.allocate_with_fifos(device, graph_buffer)
+with open(FACE_GRAPH, mode='rb') as f: face_graph_buffer = f.read()
+face_graph = mvnc.Graph('face_graph')
+input_fifo_face, output_fifo_face = face_graph.allocate_with_fifos(device, face_graph_buffer)
+
+with open(AGE_GRAPH, mode='rb') as f: age_graph_buffer = f.read()
+age_graph = mvnc.Graph('age_graph')
+input_fifo_age, output_fifo_age = age_graph.allocate_with_fifos(device, age_graph_buffer)
+
+with open(GENDER_GRAPH, mode='rb') as f: gender_graph_buffer = f.read()
+gender_graph = mvnc.Graph('gender_graph')
+input_fifo_gender, output_fifo_gender = gender_graph.allocate_with_fifos(device, gender_graph_buffer)
+
+print("Graphs allocated.")
 
 cap = cv2.VideoCapture(0)
-#cap = cv2.VideoCapture('images/benidorm1.mp4')
-#cap = VideoStream(usePiCamera=True).start()
 ret, frame = cap.read()
+print("Camera is ON.")
 
-#for showing image
+#IN ORDER TO DISPLAY IMAGE ON THE INPUT RESOLUTION
 DISPLAY_DIMS = (frame.shape[0], frame.shape[1])
-
-DISP_MULTIPLIER_Y = (DISPLAY_DIMS[0] / PREPROCESS_DIMS[0])
-DISP_MULTIPLIER_X = (DISPLAY_DIMS[1] / PREPROCESS_DIMS[1])
-
-DISP_MULT = DISP_MULTIPLIER_X, DISP_MULTIPLIER_Y
+DISP_MULTIPLIER_Y_300 = (DISPLAY_DIMS[0] / PREPROCESS_DIMS_300[0])
+DISP_MULTIPLIER_X_300 = (DISPLAY_DIMS[1] / PREPROCESS_DIMS_300[1])
+DISP_MULT_300 = DISP_MULTIPLIER_X_300, DISP_MULTIPLIER_Y_300
 
 #Initializing tracker
 ct = CentroidTracker()
+#Inicialize data_storage
+dt = Data_support.AllData()
 
-
+FRAME_COUNTER = 0
 while True:
-    startT =time.clock()
     ret, frame = cap.read()
+    FRAME_COUNTER = FRAME_COUNTER + 1
+
     
+    image_for_result = frame.copy() 
+    img = sf.pre_process_img(frame,PREPROCESS_DIMS_300)
 
-    image_for_result = frame
-    img = sf.pre_process_img(frame,PREPROCESS_DIMS)
+    person_graph.queue_inference_with_fifo_elem(input_fifo_person,output_fifo_person,img,None)
+    person_output, user_obj = output_fifo_person.read_elem()
 
-    #print("time_frame_aq: {:.0f} ms".format((time.clock()-startT)*1000))
-    start =time.clock()
+    person_predictions, boxes_person = sf.process_prediction(person_output,PREPROCESS_DIMS_300,DISP_MULT_300)
 
-    graph.queue_inference_with_fifo_elem(input_fifo, output_fifo, img, None)
-    output, user_obj = output_fifo.read_elem()
+    img_out= sf.draw_output(person_predictions,image_for_result,DISP_MULT_300)
 
-    #print("predicting: {:.0f} ms".format((time.clock()-start)*1000))
 
-    predictions, boxes = sf.process_prediction(output,PREPROCESS_DIMS,DISP_MULT)
 
-    img_out= sf.draw_output(predictions,image_for_result,DISP_MULT)
-    
     #select only person boxes
     #boxes_p = sf.person_boxes_only(predictions)
     #TRACKING STARTS HERE
-    people = ct.update(boxes)
+    people = ct.update(boxes_person)
+
+    if len(person_predictions):
+        #then search faces
+        face_graph.queue_inference_with_fifo_elem(input_fifo_face,output_fifo_face,img,None)
+        face_output, user_obj = output_fifo_face.read_elem()
+
+        face_predictions, boxes_faces = sf.process_prediction(face_output,PREPROCESS_DIMS_300,DISP_MULT_300)
+        img_out= sf.draw_output(face_predictions,image_for_result,DISP_MULT_300)
+
     for (objectID, centroid) in people.items():
-        text = "ID {}".format(objectID)
-        cv2.putText(frame, text, (int(centroid[0] - 10), int(centroid[1] - 10)),
+        
+
+        if ct.person(objectID):
+            person_is = ct.person(objectID)
+        else:
+            print("Catching person_box for ID {}".format(objectID))
+            for box in boxes_person:
+                if centroid[0] > box[0] and centroid[0] < box[2] and centroid[1] > box[1] and centroid[1] < box[3]:
+                    ct.reg_person(objectID,box)
+                else:
+                    print("No person box for ID {}".format(objectID))
+        
+        if ct.face(objectID):
+            ct.face(objectID)
+        else:
+            for boxx in boxes_faces:
+                centroid_face = (int((boxx[0] + boxx[2]) / 2.0),int((boxx[1] + boxx[3]) / 2.0))
+                if centroid_face[0] > box[0] and centroid_face[0] < box[2] and centroid_face[1] > box[1] and centroid_face[1] < box[3]:
+                    ct.reg_face(objectID,boxx)
+                else:
+                    print("No face for ID {}".format(objectID))
+
+
+
+        if ct.age(objectID):
+            age_is = ct.age(objectID)
+        if not ct.age(objectID) and ct.face(objectID):
+            
+            box_is = ct.face(objectID)
+            print(box_is)
+            try:
+                cropped_image = frame[(box_is[1]-int(box_is[1]*0.3)):(box_is[3]+int(box_is[3]*0.3)),(box_is[0]-int(box_is[0]*0.2)):(box_is[2]+int(box_is[2]*0.2))]
+                cv2.imshow('face',cropped_image)
+            except:
+                cropped_image = frame[(box_is[1]):(box_is[3]),(box_is[0]):(box_is[2])]
+                cv2.imshow('face',cropped_image)
+
+            #pre-process img in order to predict age 
+            face_img = sf.pre_process_img_2(cropped_image,PREPROCESS_DIMS_227,ilsvrc_mean)
+            
+            fc_img_c1 = face_img.copy()
+            age_graph.queue_inference_with_fifo_elem(input_fifo_age,output_fifo_age,fc_img_c1,None)
+            age_output, user_obj = output_fifo_age.read_elem()
+            age,age_prob = sf.process_age(age_output)
+           
+            age_is = age
+            ct.reg_age(objectID,age)
+
+            fc_img_c2 = face_img.copy()
+            gender_graph.queue_inference_with_fifo_elem(input_fifo_gender,output_fifo_gender,fc_img_c2,None)
+            gender_output, user_obj = output_fifo_gender.read_elem()
+            gender, gender_prob = sf.process_gender(gender_output)
+
+            gender_is = gender
+            ct.reg_gender(objectID,gender)
+        
+        if not ct.face(objectID):
+            age_is = "None"
+            gender_is = "None"
+            
+
+
+
+        text = "ID:{} Age:{} Gender: {}".format(objectID, age_is, gender_is)
+        cv2.putText(img_out, text, (int(centroid[0] - 10), int(centroid[1] - 10)),
             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        cv2.circle(frame, (int(centroid[0]), int(centroid[1])), 4, (0, 255, 0), -1)
+        cv2.circle(img_out, (int(centroid[0]), int(centroid[1])), 4, (0, 255, 0), -1)
 
 
     cv2.imshow('image',img_out)
@@ -79,9 +178,18 @@ while True:
     if key == ord("q"):
         break
 
-cap.release()
-input_fifo.destroy()
-output_fifo.destroy()
-graph.destroy()
+input_fifo_person.destroy()
+output_fifo_person.destroy()
+person_graph.destroy()
+input_fifo_face.destroy()
+output_fifo_face.destroy()
+face_graph.destroy()
+input_fifo_age.destroy()
+output_fifo_age.destroy()
+age_graph.destroy()
+input_fifo_gender.destroy()
+output_fifo_gender.destroy()
+gender_graph.destroy()
+
 device.close()
 device.destroy()
