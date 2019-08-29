e838cfeebcaea54ef7250db444992476f688f21e
Vlad <twixstar1@gmail.com>
2019-08-29 20:59:50 +0100

    changes to test + new images to test

--- speed_test.py
+++ speed_test.py
@@ -69,9 +69,12 @@ if mode == "NCS":
 
     print("Graphs allocated.")
 
-cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture(0)
 #cap = cv2.VideoCapture("images/10min18.mp4")
-ret, frame = cap.read()
+
+#ret, frame = cap.read()
+
+frame = cv2.imread('images/5.jpg')
 
 print("Camera is ON.")
 
@@ -92,13 +95,21 @@ drawi_time = []
 track_time = []
 face_time = []
 proc_inference_time_2 = []
+catch_box_for_ID_time = []
+catch_face_for_ID_time = []
+crop_and_process_time = []
+age_inference_time = []
+age_process_time = []
+gender_inference_time = []
+gender_process_time = []
+drawi_time2 = []
 
 start_time = time.time()
 FRAME_COUNTER = 0
 
 while True:
     time_0 = time.time()
-    ret, frame = cap.read()
+    #ret, frame = cap.read()
     time_00 = time.time()
     FRAME_COUNTER += 1
 
@@ -149,20 +160,24 @@ while True:
     for (objectID, centroid) in people.items():
         
 
-        if ct.person(objectID) and ct.age(objectID) and ct.gender(objectID):
+        if ct.person(objectID) and ct.age(objectID) and ct.gender(objectID) and False:
             person_is = ct.person(objectID)
         else:
             #print("Catching person_box for ID {}".format(objectID))
+            time_15 = time.time()
             for box in boxes_person:
                 if centroid[0] > box[0] and centroid[0] < box[2] and centroid[1] > box[1] and centroid[1] < box[3]:
                     ct.reg_person(objectID,box)
                 else:
                     #print("No person box for ID {}".format(objectID))
                     pass
-        
-        if ct.face(objectID) and ct.age(objectID) and ct.gender(objectID):
+            time_16 = time.time()
+
+
+        if ct.face(objectID) and ct.age(objectID) and ct.gender(objectID) and False:
             ct.face(objectID)
         else:
+            time_17 = time.time()
             for boxx in boxes_faces:
                 centroid_face = (int((boxx[0] + boxx[2]) / 2.0),int((boxx[1] + boxx[3]) / 2.0))
                 if centroid_face[0] > box[0] and centroid_face[0] < box[2] and centroid_face[1] > box[1] and centroid_face[1] < box[3]:
@@ -170,13 +185,11 @@ while True:
                 else:
                     #print("No face for ID {}".format(objectID))
                     pass
-
-
-
-        if ct.age(objectID):
-            age_is = ct.age(objectID)
-        if not ct.age(objectID) and ct.face(objectID):
+            time_18 = time.time()
             
+
+        if ct.face(objectID):
+            time_19 = time.time()
             box_is = ct.face(objectID)
             
             try:
@@ -190,32 +203,59 @@ while True:
             face_img = sf.pre_process_img_2(cropped_image,PREPROCESS_DIMS_227,ilsvrc_mean)
             
             fc_img_c1 = face_img.copy()
+
+            time_20 = time.time()
+
+            
             age_graph.queue_inference_with_fifo_elem(input_fifo_age,output_fifo_age,fc_img_c1,None)
             age_output, user_obj = output_fifo_age.read_elem()
+
+            time_21 = time.time()
+
             age,age_prob = sf.process_age(age_output)
-        
+
+            time_22 = time.time()
+            
+            
             age_is = age
             ct.reg_age(objectID,age)
 
             fc_img_c2 = face_img.copy()
+
+            time_23 = time.time()
             gender_graph.queue_inference_with_fifo_elem(input_fifo_gender,output_fifo_gender,fc_img_c2,None)
             gender_output, user_obj = output_fifo_gender.read_elem()
+            time_24 = time.time()
+
             gender, gender_prob = sf.process_gender(gender_output)
+            time_25 = time.time()
 
             gender_is = gender
             ct.reg_gender(objectID,gender)
-            print("ID:{} Age:{} Gender: {}".format(objectID, age_is, gender_is))
+            #print("ID:{} Age:{} Gender: {}".format(objectID, age_is, gender_is))
         
         if not ct.face(objectID):
             age_is = "None"
             gender_is = "None"
             
-
+        time_26 = time.time()
         #text = "ID:{} Age:{} Gender: {}".format(objectID, age_is, gender_is)
         text = "ID:{}".format(objectID)
         cv2.putText(img_out, text, (int(centroid[0] - 10), int(centroid[1] - 10)),
             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
         cv2.circle(img_out, (int(centroid[0]), int(centroid[1])), 4, (0, 255, 0), -1)
+
+        time_27 = time.time()
+
+
+        catch_box_for_ID_time.append(time_16 - time_15)
+        catch_face_for_ID_time.append(time_18 - time_17)
+        crop_and_process_time.append(time_20 - time_19)
+        age_inference_time.append(time_21 - time_20)
+        age_process_time.append(time_22 - time_21)
+        gender_inference_time.append(time_24 - time_23)
+        gender_process_time.append(time_25 - time_24)
+        drawi_time2.append(time_27 - time_26)
 
 
     cv2.imshow('image',img_out)
@@ -236,33 +276,57 @@ while True:
     if FRAME_COUNTER == int(frames_limit) or key == ord("q"):
         break
 
-print("Done {} frames in {:.3f} seconds or {:.1f} FPS, {:.3f} seconds per image".format(FRAME_COUNTER,(time.time()-start_time),FRAME_COUNTER/(time.time()-start_time),(time.time()-start_time)/FRAME_COUNTER))
+print("Done {} frames in {:.3f} seconds or {:.1f} FPS, {:.1f} ms per image".format(FRAME_COUNTER,(time.time()-start_time),FRAME_COUNTER/(time.time()-start_time),(1000*(time.time()-start_time)/FRAME_COUNTER)))
 
 captur_time_ = sum(captur_time,0)/len(captur_time)
-print("Capturing time: {:.3f}".format(captur_time_))
+print("Capturing time: {:.1f} ms".format(captur_time_*1000))
 
 proc_time_ = sum(processing_time,0)/len(processing_time)
-print("Processig time: {:.3f}".format(proc_time_))
+print("Processig time(Rsize and normalization): {:.1f} ms".format(proc_time_*1000))
 
 inf1_time_ = sum(inf1_time,0)/len(inf1_time)
-print("People inference time: {:.3f}".format(inf1_time_))
+print("People inference time: {:.1f} ms".format(inf1_time_*1000))
 
 proc_inference_time_ = sum(proc_inference_time,0)/len(proc_inference_time)
-print("Preocessing inference time: {:.3f}".format(proc_inference_time_))
+print("Preocessing inference time: {:.1f} ms".format(proc_inference_time_*1000))
 
 drawi_time_ = sum(drawi_time,0)/len(drawi_time)
-print("Drawing time_1 {:.3f}".format(drawi_time_))
+print("Drawing time_1: {:.1f} ms".format(drawi_time_*1000))
 
 track_time_ = sum(track_time,0)/len(track_time)
-print("Tracking time {:.3f}".format(track_time_))
+print("Tracking time: {:.1f} ms".format(track_time_*1000))
 
 face_time_ = sum(face_time,0)/len(face_time)
-print("Face inference time {:.3f}".format(face_time_))
+print("Face inference time: {:.1f} ms".format(face_time_*1000))
 
 proc_inference_time_2_ = sum(proc_inference_time_2,0)/len(proc_inference_time_2)
-print("Face inference time {:.3f}".format(proc_inference_time_2_))
+print("Processig inference time 2: {:.1f} ms".format(proc_inference_time_2_*1000))
 
-cap.release()
+catch_box_for_ID_time_ = sum(catch_box_for_ID_time,0)/len(catch_box_for_ID_time)
+print("Catch box for ID time: {:.1f} ms".format(catch_box_for_ID_time_*1000))
+
+catch_face_for_ID_time_ = sum(catch_face_for_ID_time,0)/len(catch_face_for_ID_time)
+print("Catch face for ID time: {:.1f} ms".format(catch_face_for_ID_time_*1000))
+
+crop_and_process_time_ = sum(crop_and_process_time,0)/len(crop_and_process_time)
+print("Crop face and process time: {:.1f} ms".format(crop_and_process_time_*1000))
+
+age_inference_time_ = sum(age_inference_time,0)/len(age_inference_time)
+print("Age inference time: {:.1f} ms".format(age_inference_time_*1000))
+
+age_process_time_ = sum(age_process_time,0)/len(age_process_time)
+print("Age process time: {:.1f} ms".format(age_process_time_*1000))
+
+gender_inference_time_ = sum(gender_inference_time,0)/len(gender_inference_time)
+print("Gender inference time: {:.1f} ms".format(gender_inference_time_*1000))
+
+gender_process_time_ = sum(gender_process_time,0)/len(gender_process_time)
+print("Gender process time: {:.1f} ms".format(gender_process_time_*1000))
+
+drawi_time2_ = sum(drawi_time2,0)/len(drawi_time2)
+print("Last drawing time: {:.1f} ms".format(drawi_time2_*1000))
+
+#cap.release()
 
 if mode == "NCS":
     input_fifo_person.destroy()
